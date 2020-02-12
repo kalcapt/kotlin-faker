6b0fab13aff9d042617132d77cff922ae8e05ff3
Sergio Pro <sergio.igwt@gmail.com>
2020-02-12 14:27:34 +0100

    Split project into cli and core modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-import org.gradle.api.tasks.testing.logging.*
-import org.jetbrains.kotlin.gradle.tasks.*
-import java.util.*
-
 plugins {
     kotlin("jvm") version "1.3.61"
     `maven-publish`
@@ -9,194 +5,113 @@ plugins {
     id("io.qameta.allure") version "2.8.1"
 }
 
-group = properties["GROUP"].toString()
-version = properties["VERSION"].toString()
+subprojects {
+    group = properties["GROUP"].toString()
+    version = properties["VERSION"].toString()
 
-repositories {
-    jcenter()
-    mavenCentral()
-}
+    buildscript {
+        repositories {
+            maven("https://plugins.gradle.org/m2/")
+        }
+        dependencies {
+            classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
+        }
+    }
 
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("reflect"))
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.1")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.10.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1")
-    implementation("com.github.mifmif:generex:1.0.2")
-    implementation("io.github.classgraph:classgraph:4.8.56")
-    implementation("org.slf4j:slf4j-api:1.7.28")
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
-    testImplementation("io.kotlintest:kotlintest-extensions-allure:3.4.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-    testRuntimeOnly("ch.qos.logback:logback-core:1.2.3")
-    testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
-    testRuntimeOnly("org.codehaus.groovy:groovy:2.5.6")
-    runtimeOnly(kotlin("script-runtime"))
-}
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+    }
 
-tasks {
-    jar {
+    repositories {
+        jcenter()
+        mavenCentral()
+    }
+
+    dependencies {
+        val implementation by configurations
+        val testImplementation by configurations
+        val testRuntimeOnly by configurations
+        val runtimeOnly by configurations
+
+        implementation(kotlin("stdlib-jdk8"))
+        implementation(kotlin("reflect"))
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.10.1")
+        implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.10.1")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1")
+        implementation("com.github.mifmif:generex:1.0.2")
+        implementation("io.github.classgraph:classgraph:4.8.56")
+        implementation("org.slf4j:slf4j-api:1.7.28")
+        testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
+        testImplementation("io.kotlintest:kotlintest-extensions-allure:3.4.2")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
+        testRuntimeOnly("ch.qos.logback:logback-core:1.2.3")
+        testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+        testRuntimeOnly("org.codehaus.groovy:groovy:2.5.6")
+        runtimeOnly(kotlin("script-runtime"))
+    }
+
+    tasks.withType<Jar> {
         manifest {
             attributes(
                 mapOf(
                     "Implementation-Title" to project.name,
                     "Implementation-Version" to project.version,
-                    "Class-Path" to configurations.compileClasspath.get().joinToString(" ") { it.name }
+                    "Class-Path" to this@subprojects.configurations.compileClasspath.get().joinToString(" ") { it.name }
                 )
             )
         }
+
+        archiveClassifier.set("sources")
+        from(sourceSets.getByName("main").allSource)
+        from("LICENCE.md") {
+            into("META-INF")
+        }
     }
-}
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 
-tasks.withType<Wrapper> {
-    this.gradleVersion = "6.0.1"
-    this.distributionType = Wrapper.DistributionType.ALL
-}
+    tasks.withType<Wrapper> {
+        this.gradleVersion = "6.0.1"
+        this.distributionType = Wrapper.DistributionType.ALL
+    }
 
-val test by tasks.getting(Test::class) {
-    useJUnitPlatform {}
+    val test by tasks.getting(Test::class) {
+        useJUnitPlatform {}
 
-    // show standard out and standard error of the test JVM(s) on the console
-    testLogging.showStandardStreams = true
+        // show standard out and standard error of the test JVM(s) on the console
+        testLogging.showStandardStreams = true
 
-    // Always run tests, even when nothing changed.
-    dependsOn("cleanTest")
+        // Always run tests, even when nothing changed.
+        dependsOn("cleanTest")
 
-    testLogging {
-        showStandardStreams = true
-        events(
-            TestLogEvent.FAILED,
-            TestLogEvent.SKIPPED,
-            TestLogEvent.STANDARD_OUT,
-            TestLogEvent.STANDARD_ERROR
-        )
-        exceptionFormat = TestExceptionFormat.FULL
-        showExceptions = true
-        showCauses = true
-        showStackTraces = true
-
-        debug {
+        testLogging {
+            showStandardStreams = true
             events(
-                TestLogEvent.FAILED,
-                TestLogEvent.SKIPPED,
-                TestLogEvent.STANDARD_OUT,
-                TestLogEvent.STANDARD_ERROR
+                org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
             )
-            exceptionFormat = TestExceptionFormat.FULL
-        }
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+            showExceptions = true
+            showCauses = true
+            showStackTraces = true
 
-        info.events = debug.events
-        info.exceptionFormat = debug.exceptionFormat
-    }
-}
-
-allure {
-    version = "2.8.1"
-    aspectjweaver = false
-    autoconfigure = true
-    allureJavaVersion = "2.12.1"
-    useJUnit5 {
-        version = "2.12.1"
-    }
-}
-
-val sourcesJar by tasks.creating(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets.getByName("main").allSource)
-    from("LICENCE.md") {
-        into("META-INF")
-    }
-}
-
-val artifactName = project.name
-val artifactGroup = project.group.toString()
-val artifactVersion = project.version.toString()
-
-val pomUrl = "https://github.com/serpro69/kotlin-faker"
-val pomScmUrl = "https://github.com/serpro69/kotlin-faker"
-val pomIssueUrl = "https://github.com/serpro69/kotlin-faker/issues"
-val pomDesc = "https://github.com/serpro69/kotlin-faker"
-
-val githubRepo = "serpro69/kotlin-faker"
-val githubReadme = "README.md"
-
-val pomLicenseName = "MIT"
-val pomLicenseUrl = "https://opensource.org/licenses/mit-license.php"
-val pomLicenseDist = "repo"
-
-val pomDeveloperId = "serpro69"
-val pomDeveloperName = "Sergii Prodanov"
-
-
-publishing {
-    publications {
-        create<MavenPublication>("kotlin-faker") {
-            groupId = artifactGroup
-            artifactId = artifactName
-            version = artifactVersion
-            from(components["java"])
-            artifact(sourcesJar)
-
-            pom {
-                packaging = "jar"
-                name.set(rootProject.name)
-                description.set(pomDesc)
-                url.set(pomUrl)
-                scm {
-                    url.set(pomScmUrl)
-                }
-                issueManagement {
-                    url.set(pomIssueUrl)
-                }
-                licenses {
-                    license {
-                        name.set(pomLicenseName)
-                        url.set(pomLicenseUrl)
-                    }
-                }
-                developers {
-                    developer {
-                        id.set(pomDeveloperId)
-                        name.set(pomDeveloperName)
-                    }
-                }
+            debug {
+                events(
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
+                )
+                exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
             }
-        }
-    }
-}
 
-bintray {
-    user = project.findProperty("bintrayUser").toString()
-    key = project.findProperty("bintrayKey").toString()
-    publish = true
-
-    setPublications("kotlin-faker")
-
-    pkg.apply {
-        repo = "maven"
-        name = artifactName
-        userOrg = "serpro69"
-        githubRepo = githubRepo
-        vcsUrl = pomScmUrl
-        description = "Port of ruby faker gem written in kotlin"
-        setLabels("kotlin", "faker", "testing", "test-automation", "data", "generation")
-        setLicenses("MIT")
-        desc = description
-        websiteUrl = pomUrl
-        issueTrackerUrl = pomIssueUrl
-        githubReleaseNotesFile = githubReadme
-
-        version.apply {
-            name = artifactVersion
-            desc = pomDesc
-            released = Date().toString()
-            vcsTag = artifactVersion
+            info.events = debug.events
+            info.exceptionFormat = debug.exceptionFormat
         }
     }
 }
